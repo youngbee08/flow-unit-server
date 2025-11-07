c9e6350ba8ed46f5c089c0967e937778b2558bf0
youngbee08 <youngbeeh08@gmail.com>
2025-11-07 17:14:52 +0100

    edited smtp configuration

--- utils/nodemailer/transporter.js
+++ utils/nodemailer/transporter.js
@@ -1,24 +1,26 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
 dotenv.config();
 const serverPassword = process.env.SERVER_PASS;
 const serverMail = process.env.SERVER_MAIL;
 
 const transporter = nodemailer.createTransport({
-  host:'smtp.gmail.com',
-  port:587,
-  secure:false,
-  auth:{
-    user:serverMail,
-    pass:serverPassword
-  }
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: serverMail,
+    pass: serverPassword,
+  },
 });
 
-transporter.verify((err,success) =>{
+transporter.verify((err, success) => {
   if (success) {
-    console.log("Server ready to send mail")
-  } else{
-    console.log(`An error occured while trying to set up mail transporter, Error:${err}`)
+    console.log("Server ready to send mail");
+  } else {
+    console.log(
+      `An error occured while trying to set up mail transporter, Error:${err}`
+    );
   }
 });
 
